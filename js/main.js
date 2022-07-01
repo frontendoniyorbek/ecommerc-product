716db5163e6d06fda98276233a0fd6d01a0a67b2
frontendoniyorbek <tursunovdoniyorbek077@gmail.com>
2022-07-01 22:43:16 +0300

    js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+const modifiers = {
+  ImgThumbnailActive: 'img-showcase__thumbnail--active'
+};
+
 const elSiteHeaderCartLink = document.querySelector('.js-site-header-cart-link');
 const elSiteHeaderCartModal = document.querySelector('.site-header__cart-modal');
 
@@ -9,3 +13,20 @@ if (elSiteHeaderCartLink) {
     elSiteHeaderCartModal.classList.toggle('site-header__cart-modal--open')
   })
 }
+
+
+//Image showcase
+
+const elsImgShowcaseThumbnailsButton = document.querySelectorAll('.js-img-showcase-thumbnails-button');
+const elsImgThumbnail = document.querySelectorAll('.img-showcase__thumbnail');
+
+elsImgShowcaseThumbnailsButton.forEach(function (elButton) {
+  elButton.addEventListener('click' , function () {
+    // Removeactive state from all buttons
+    elsImgThumbnail.forEach(function (elImgThumbnail) {
+      elImgThumbnail.classList.remove(modifiers.ImgThumbnailActive)
+    });
+    //Add active state to clicked button
+    elButton.parentElement.classList.add(modifiers.ImgThumbnailActive)
+  });
+});
